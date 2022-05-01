c00ab72b3a047fcc470f8d18ffb4255a29dc1abd
honbey <git@honbey.com>
2022-05-01 22:22:47 +0800

    chorn: update

--- certbot_renew.sh
+++ certbot_renew.sh
@@ -2,15 +2,13 @@
 
 # 0 2 1 * * /path/certbot_renew.sh >> /opt/web/logs/ddns/certbot-renew.log 2>&1
 
-# certbot certonly --manual --preferred-challenges dns-01 --email r@honbey.com -d 'freewisdom.cn' -d '*.freewisdom.cn' --server https://acme-v02.api.letsencrypt.org/directory --manual-auth-hook /opt/zhang/certbot-auth-dnspod/certbot-auth-dnspod.sh --manual-cleanup-hook "/opt/zhang/certbot-auth-dnspod/certbot-auth-dnspod.sh clean" --config-dir /opt/zhang/letsencrypt --work-dir /opt/zhang/letsencrypt --logs-dir /opt/zhang/logs/letsencrypt/
-
 date
 
-/opt/zhang/pyvenv/certbot/bin/certbot renew \
-  --manual-auth-hook /opt/zhang/certbot-auth-dnspod/certbot-auth-dnspod.sh \
-  --manual-cleanup-hook "/opt/zhang/certbot-auth-dnspod/certbot-auth-dnspod.sh clean" \
-  --config-dir /opt/zhang/letsencrypt \
-  --work-dir /opt/zhang/letsencrypt \
-  --logs-dir /opt/zhang/logs/letsencrypt
+/opt/data/pyvenv/certbot/bin/certbot renew \
+  --manual-auth-hook /opt/data/certbot-auth-dnspod/certbot-auth-dnspod.sh \
+  --manual-cleanup-hook "/opt/data/certbot-auth-dnspod/certbot-auth-dnspod.sh clean" \
+  --config-dir /opt/data/letsencrypt \
+  --work-dir /opt/data/letsencrypt \
+  --logs-dir /opt/data/logs/letsencrypt
 
 /usr/local/nginx-quic/sbin/nginx -s reload
